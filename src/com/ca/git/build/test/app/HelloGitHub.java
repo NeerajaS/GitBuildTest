561b67b3c0820a2bebb151525922f236880ae5db
NeerajaS <neerajasurisetty@gmail.com>
2015-07-22 15:57:49 +0530

    First commit

---
+++ src/com/ca/git/build/test/app/HelloGitHub.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.ca.git.build.test.app;
+
+/**
+ * @author surne01
+ *
+ */
+public class HelloGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello to GitHub");
+	}
+
+}
